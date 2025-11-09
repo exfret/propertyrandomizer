20c0bcc8fd36bd1c1286f3daf1e1b5c8204956cc
Nifyr <nintendofyr@gmail.com>
2025-11-09 13:48:37 +0100

    Modify technology randomization

--- randomizations/numerical/technology.lua
+++ randomizations/numerical/technology.lua
@@ -7,7 +7,6 @@ local randomize = randnum.rand
 -- TODO: Research trigger amounts
 -- TODO: Research bonus amounts (like extra bullet damage)
 
--- Biased toward lower amounts to make expected average closer to normal
 randomizations.tech_costs = function(id)
     for _, tech in pairs(data.raw.technology) do
         if tech.unit ~= nil then
@@ -20,20 +19,19 @@ randomizations.tech_costs = function(id)
                     tbl = tech.unit,
                     property = "count",
                     abs_min = 1,
-                    bias = 0.05,
-                    dir = -1
+                    dir = -1,
+                    rounding = "discrete"
                 })
 
                 tech.localised_description = locale_utils.create_localised_description(tech, tech.unit.count / old_count, id, {flipped = true})
             else
                 -- In this case, we have a count formula
-                local key = rng.key({id = id, prototype = prototype})
+                local key = rng.key({id = id, prototype = tech})
                 local formula_multiplier = randomize({
                     key = key,
                     dummy = 1,
-                    abs_min = 1,
-                    bias = 0.05,
-                    dir = -1
+                    dir = -1,
+                    rounding = "discrete_float"
                 })
                 tech.unit.count_formula = formula_multiplier .. "*(" .. tech.unit.count_formula .. ")"
 
@@ -54,9 +52,8 @@ randomizations.tech_times = function(id)
                 tbl = tech.unit,
                 property = "time",
                 abs_min = 1,
-                bias = 0.05,
                 dir = -1,
-                rounding = "discrete"
+                rounding = "discrete_float"
             })
 
             tech.localised_description = locale_utils.create_localised_description(tech, tech.unit.time / old_time, id, {flipped = true})
