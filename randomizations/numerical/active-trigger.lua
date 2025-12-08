634c8fc3b1e6ad1ff2aca69b52108b67a8ec5644
Nifyr <nintendofyr@gmail.com>
2025-12-08 16:54:58 +0100

    Refactor child prototype randomizations

--- randomizations/numerical/active-trigger.lua
+++ randomizations/numerical/active-trigger.lua
@@ -1,89 +1,65 @@
 local randnum = require("lib/random/randnum")
 local randprob = require("lib/random/randprob")
-local trigger_utils = require("lib/trigger")
+local indirect = require("randomizations/helper/indirect")
 local locale_utils = require("lib/locale")
 
 
 -- New
 randomizations.chain_max_jumps = function (id)
-    local chains = trigger_utils.get_creator_table("chain-active-trigger")
-
-    local target_classes = {
-        ["ammo"] = true,
-        ["electric-turret"] = true,
-    }
-
-    for chain_name, creators in pairs(chains) do
-        local affected_prototypes = {}
-
-        for _, prototype in pairs(creators) do
-            if target_classes[prototype.type] ~= nil then
-                affected_prototypes[#affected_prototypes+1] = prototype
-            end
+    indirect.iterate_child_prototypes("chain-active-trigger", function (chain, parents, structs, is_enemy)
+        local dir = 1
+        if is_enemy then
+            dir = -1
         end
 
-        if #affected_prototypes > 0 then
-            local chain = data.raw["chain-active-trigger"][chain_name]
-            if chain.max_jumps == nil then
-                chain.max_jumps = 5
-            end
-
-            local old_value = chain.max_jumps
-
-            randnum.rand({
-                id = id,
-                prototype = chain,
-                property = "max_jumps",
-                rounding = "discrete",
-                variance = "small",
-            })
-
-            local factor = chain.max_jumps / old_value
-
-            for _, prototype in pairs(affected_prototypes) do
-                locale_utils.create_localised_description(prototype, factor, id, { variance = "small" })
-            end
+        if chain.max_jumps == nil then
+            chain.max_jumps = 5
         end
-    end
+
+        local old_value = chain.max_jumps
+
+        randnum.rand({
+            id = id,
+            prototype = chain,
+            property = "max_jumps",
+            rounding = "discrete",
+            variance = "small",
+            dir = dir,
+        })
+
+        local factor = chain.max_jumps / old_value
+
+        for _, parent in pairs(parents) do
+            locale_utils.create_localised_description(parent, factor, id, { flipped = dir < 0, variance = "small" })
+        end
+    end)
 end
 
 -- New
 randomizations.chain_fork_chance = function (id)
-    local chains = trigger_utils.get_creator_table("chain-active-trigger")
+    indirect.iterate_child_prototypes("chain-active-trigger", function (chain, parents, structs, is_enemy)
+        if chain.fork_chance ~= nil and chain.fork_chance > 0 then
+            local dir = 1
+            if is_enemy then
+                dir = -1
+            end
 
-    local target_classes = {
-        ["ammo"] = true,
-        ["electric-turret"] = true,
-    }
+            local old_value = chain.fork_chance
 
-    for chain_name, creators in pairs(chains) do
-        local affected_prototypes = {}
+            randprob.rand({
+                id = id,
+                prototype = chain,
+                property = "fork_chance",
+                rounding = "discrete_float",
+                variance = "small",
+                dir = dir,
+            })
 
-        for _, prototype in pairs(creators) do
-            if target_classes[prototype.type] ~= nil then
-                affected_prototypes[#affected_prototypes+1] = prototype
+            local factor = randprob.calc_odds_factor(old_value, chain.fork_chance)
+
+            for _, prototype in pairs(parents) do
+                locale_utils.create_localised_description(prototype, factor, id, { flipped = dir < 0, variance = "small" })
             end
         end
-
-        if #affected_prototypes > 0 then
-            local chain = data.raw["chain-active-trigger"][chain_name]
-            if chain.fork_chance ~= nil and chain.fork_chance > 0 then
-                local old_value = chain.fork_chance
-
-                randprob.rand({
-                    id = id,
-                    prototype = chain,
-                    property = "fork_chance",
-                    rounding = "discrete_float",
-                    variance = "small",
-                })
-
-                local factor = randprob.calc_odds_factor(old_value, chain.fork_chance)
-
-                for _, prototype in pairs(affected_prototypes) do
-                    locale_utils.create_localised_description(prototype, factor, id, { variance = "small" })
-                end
-            end
-        end
-    end
+    end)
 end
